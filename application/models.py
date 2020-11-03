e199d3bcec27028eccef48a78234fe9290105c80
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-11-03 21:14:42 +0000

    Added structure and files

---
+++ application/models.py
@@ -0,0 +1,18 @@
+from application import db
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    platform = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    games= = db.relationship('Sessions', backref='game')
+
+    
+
+
+class Sessions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    time_played = db.Column(db.Integer, nullable=False)
+    date_played = db.Column(db.DateTime, nullable=False)
