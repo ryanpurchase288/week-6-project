0efa76330f2886e0d55fe400084e7d4d6e81e3ce
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-11-09 16:00:24 +0000

    updated to allow to add a new game

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, redirect, url_for, request
 from application import app, db
-from application.models import ToDoList
-from application.forms import Game  
+from application.models import Game
+from application.forms import GameForm  
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     return render_template('index.html', gameList = Game.query.all())
+
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    form = GameForm()
+    if form.validate_on_submit():
+        new_game= Game(name = form.game.data, platform = form.platform.data)
+        db.session.add(new_game
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('addgame.html', form=form)
