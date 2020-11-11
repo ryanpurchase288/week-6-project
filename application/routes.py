e32961b2dcdda505dabffe51824f36cb205cf983
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-11-11 14:56:10 +0000

    completed crud of my game table

--- application/routes.py
+++ application/routes.py
@@ -17,3 +17,28 @@ def add():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('addgame.html', form=form)
+
+@app.route('/update/<idNum>', methods=['POST', 'GET'])
+def update(idNum):
+    form = GameForm()
+    game = Game.query.get(idNum)
+    if form.validate_on_submit():
+        game.name = form.game.data
+        game.platform = form.platform.data
+        db.session.commit()
+        return redirect(url_for('index'))
+    elif request.method == 'GET':
+        form.game.data = game.name
+        form.platform.data = game.platform
+    return render_template('updategame.html', title='Update your game', form=form)
+
+
+@app.route('/delete/<idNum>')
+def delete(idNum):
+    game= Game.query.get(idNum)
+    db.session.delete(game)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+
