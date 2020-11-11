0c3ab6b6b2e5c215d24f89c35f8453b221405197
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-11-11 15:06:15 +0000

    added some validators

--- application/forms.py
+++ application/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,  SubmitField, IntegerField, DateField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, Length
 from application.models import Game, Sessions
 
 
 class GameForm(FlaskForm):
-    game =  StringField('Name:', validators = [DataRequired()])
-    platform =  StringField('Platform', validators = [DataRequired()])
+    game =  StringField('Name:', validators = [DataRequired(),
+            Length(min=3, max=60, message='your name is too short or long')]
+    platform =  StringField('Platform', validators = [DataRequired(),
+        Length(min=3, max=60, message='your name is too short or long')])
     submit = SubmitField('Submit')
 
 class SessionForm(FlaskForm):
