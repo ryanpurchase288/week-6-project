5a59432925d6ac2828199c2318cc3eab69b66871
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-11-12 12:24:18 +0000

    Tests completed up to 84% coverage

--- tests/test_app.py
+++ tests/test_app.py
@@ -66,3 +66,43 @@ class TestViews(TestBase):
     def test_view_get(self):
         response = self.client.get(url_for('viewSession', idNum=1))
         self.assertEqual(response.status_code,200)
+    
+    def test_addsession_get(self):
+        response = self.client.get(url_for('addsession', idNum=1))
+        self.assertEqual(response.status_code,200)
+
+    def test_updatesession_get(self):
+        response = self.client.get(url_for('updatesession', idNum=1))
+        self.assertEqual(response.status_code,200)
+
+class TestAdd(TestBase):
+    def test_add_game(self):
+        game = Game(
+            name='GTA',
+            platform='PS5'
+        )
+        db.session.add(game)
+        db.session.commit()
+        self.assertEqual(Game.query.count(), 2)
+    
+    def test_add_session(self):
+        session = Sessions(
+            game_id=1,
+            time_played=234,
+            date_played=datetime(2020,11,12)
+        )
+        db.session.add(session)
+        db.session.commit()
+        self.assertEqual(Sessions.query.count(),2)
+
+class TestUpdate(TestBase):
+    def test_update_post(self):
+        response = self.client.post(
+            url_for('update', idNum=1),
+            data = dict(game="Pokemon", platform="3DS"),
+            follow_redirects=True
+            )
+        self.assertIn(b'Pokemon', response.data)
+        self.assertIn(b'3DS', response.data)
+
+
