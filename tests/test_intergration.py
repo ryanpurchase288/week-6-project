ef28c2d9f55ae0aaaafe6d00f7bdbe8f79a9889e
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-11-13 10:19:02 +0000

    started selenium testing

---
+++ tests/test_intergration.py
@@ -0,0 +1,44 @@
+import unittest
+import time
+from flask import url_for
+from urllib.request import urlopen
+
+from os import getenv
+from flask_testing import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from application import app, db
+from application.models import Game, Sessions
+from datetime import datetime
+
+
+
+class TestBase(LiveServerTestCase):
+
+    def create_app(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+        app.config['SECRET_KEY'] = 'scnbuidsbcduisb'
+        return app
+
+    def setUp(self):
+        """Setup the test driver and create test users"""
+        print("--------------------------NEXT-TEST----------------------------------------------")
+        chrome_options = Options()
+        chrome_options.binary_location = "/usr/bin/chromium-browser"
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\Google\Chrome\Application", chrome_options=chrome_options)
+        self.driver.get("http://localhost:5000")
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+    def tearDown(self):
+        self.driver.quit()
+        print("--------------------------END-OF-TEST----------------------------------------------\n\n\n-------------------------UNIT-AND-SELENIUM-TESTS----------------------------------------------")
+
+    def test_server_is_up_and_running(self):
+        response = urlopen("http://localhost:5000")
+        self.assertEqual(response.code, 200)
+
+if __name__ == '__main__':
+    unittest.main(port=5000)
